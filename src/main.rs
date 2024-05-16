165c18abcef3b94553cc9b7d6e5a9947f94d1e31
Anna Dunster <anna@annardunster.com>
2024-05-15 21:07:51 -0700

    Convert to cli argument for string

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::env;
 use tiny_mystery_game::names::{get_first_name, get_surname};
 use tiny_mystery_game::rng::{calculate_hash, coin_flip};
 use tiny_mystery_game::villagers::Gender;
 
 fn main() {
-    let stringy_seed = "some_seedz";
+    let args: Vec<String> = env::args().collect();
+
+    let stringy_seed = if args.len() >= 3 && args[1] == "seed" {
+        &args[2]
+    } else {
+        "some_seedz"
+    };
 
     for position in 0..3 {
         let seed_with_pos = stringy_seed.to_owned() + &position.to_string();
