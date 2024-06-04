bc26a4a8cfd20d1d3012ee2dc8895ff027a4bbc5
Anna Dunster <anna@annardunster.com>
2024-06-04 16:40:13 -0700

    Begin indexing sprites

--- src/tiles/mod.rs
+++ src/tiles/mod.rs
@@ -1,3 +1,6 @@
+mod tile_index;
+
+use crate::tiles::tile_index::TileIndex;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
@@ -39,7 +42,7 @@ pub fn spawn_tile_sprite(
     commands.spawn(SpriteSheetBundle {
         atlas: TextureAtlas {
             layout: sprite_atlas.0.clone(),
-            index: 2,
+            index: TileIndex::PersonHelmetSpearShield as usize,
         },
         texture: tile_texture,
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
