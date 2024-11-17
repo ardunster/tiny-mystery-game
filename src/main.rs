a074d3e580583c99fa4ff3fdce171b79527fd843
Anna Dunster <anna@annardunster.com>
2024-11-17 11:42:13 -0800

    Adjust log output in playground

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() -> AppExit {
         .add_plugins(
             DefaultPlugins
                 .set(LogPlugin {
-                    filter: "info,Playground=debug".into(),
+                    filter: "info,Playground::Seed=debug".into(),
                     level: bevy::log::Level::DEBUG,
                     ..default()
                 })
@@ -41,19 +41,19 @@ fn playground(env_args: Res<EnvArgsResource>) {
     let args = &env_args.args;
 
     let stringy_seed = if args.len() >= 3 && args[1] == "seed" {
-        debug!(target: "Playground", "Found seed CLI argument: {}", args[2]);
+        debug!(target: "Playground::Seed", "Found seed CLI argument: {}", args[2]);
         &args[2]
     } else {
-        debug!(target: "Playground", "No seed argument found, using default.");
+        debug!(target: "Playground::Seed", "No seed argument found, using default.");
         "some_seedz"
     };
 
-    debug!(target: "Playground", "Resulting seed: {}", stringy_seed);
+    debug!(target: "Playground::Seed", "Resulting seed: {}", stringy_seed);
 
     for position in 0..3 {
         let seed_with_pos = stringy_seed.to_owned() + &position.to_string();
 
-        debug!(target: "Playground", "seed with position: {}", seed_with_pos);
+        debug!(target: "Playground::Villager", "seed with position: {}", seed_with_pos);
         let hash = calculate_hash(&seed_with_pos);
 
         let gender = match coin_flip(&hash) {
@@ -61,7 +61,7 @@ fn playground(env_args: Res<EnvArgsResource>) {
             false => Gender::Female,
         };
 
-        debug!(target: "Playground",
+        debug!(target: "Playground::Villager",
             "Got a name: {} {}",
             get_first_name(&hash, &gender),
             get_surname(&hash)
