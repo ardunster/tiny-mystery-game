75311fd715b6673381a7ffc1b549c3b1f8c5555e
Anna Dunster <anna@annardunster.com>
2026-02-01 15:35:53 -0800

    Extract family generation

--- src/village/generation/mod.rs
+++ src/village/generation/mod.rs
@@ -1,2 +1,3 @@
+pub mod family;
 pub mod village;
 pub mod villager;
--- src/village/generation/village.rs
+++ src/village/generation/village.rs
@@ -1,8 +1,7 @@
-use crate::names::get_surname;
 use crate::resources::WorldSeed;
 use crate::rng::hash::calculate_hash;
 use crate::rng::position_in_range::position_in_range;
-use crate::village::generation::villager;
+use crate::village::generation::family;
 use crate::village::generation::villager::VillagerGenerationPlugin;
 use crate::village::model::family::{
     Family, FamilyMembers, HouseholdSize, Surname,
@@ -81,64 +80,17 @@ fn generate_village_on_request(
     debug!(target: "Village::Generate", "Family Count = {}", family_count);
 
     for family_index in 0..family_count {
-        debug!(target: "Family::Generate", "Generating Family Index {}", family_index);
-
-        let family_seed = format!("{}:family:{}", world_seed, family_index);
-        debug!(target: "Family::Generate", "Family Seed: {}", family_seed);
-
-        let family_hash = calculate_hash(&family_seed);
-
-        let surname = get_surname(&family_hash);
-        debug!(target: "Family::Generate", "Surname: {}", surname);
-
-        let family_size = position_in_range(
-            &config.family_size_min,
-            &config.family_size_max,
-            &family_hash,
+        family::generate_family(
+            &mut commands,
+            &config,
+            world_seed,
+            family_index,
         );
-        debug!(target: "Family::Generate", "Family Size: {}", family_size);
-
-        let family_entity = commands
-            .spawn((
-                Family,
-                Surname(surname),
-                HouseholdSize(family_size),
-                FamilyMembers::default(),
-            ))
-            .id();
-
-        debug!(target: "Family::Generate", "Family Entity: {}", family_entity);
-
-        let mut family_member_entities: Vec<Entity> =
-            Vec::with_capacity(family_size as usize);
-
-        for family_member_index in 0..family_size {
-            let is_head = family_member_index == 0;
-
-            // let mut is_spouse = false;
-            // if (family_member_index == 1) {
-            //
-            // }
-
-            let new_villager = villager::generate_villager(
-                &mut commands,
-                &family_seed,
-                family_member_index,
-                family_entity,
-                is_head,
-            );
-
-            family_member_entities.push(new_villager);
-        }
-
-        commands
-            .entity(family_entity)
-            .insert(FamilyMembers(family_member_entities));
     }
 }
 
 fn debug_families_on_generate(
-    mut event_reader: EventReader<GenerateVillage>,
+    event_reader: EventReader<GenerateVillage>,
     families: Query<
         (Entity, &Surname, &HouseholdSize, &FamilyMembers),
         With<Family>,
